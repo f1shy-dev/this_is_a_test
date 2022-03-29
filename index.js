3b0fb0ef6e158a4fc1a482fc0d3afd5f2b15a372
f1shy-dev <agarwal.vrishank@gmail.com>
2022-03-29 18:30:45 +0100

    testing something

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+// return a html element on /api
+app.get("/api", (req, res) => {
+  res.send(
+    "<h1>If you're seeing this, then you got data from a localhost api, and your browser is confusing to me!</h1>"
+  );
+});
+
+app.listen(2123);
